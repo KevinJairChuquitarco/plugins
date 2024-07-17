edce3960af7cada256ca67c67e668b100442c1c6
KevinJair11 <kevinchuquitarco11@gmail.com>
2024-07-16 22:32:20 -0500

    Camera IONIC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  public photo = "";
+  constructor(private camera: Camera) { }
 
-  constructor() {}
 
+  takePicture() {
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      // La imagen está en base64
+      this.photo = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      // Manejo de error
+      console.log("error")
+    });
+  }
 }
